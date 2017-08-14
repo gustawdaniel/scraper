3ec1dc6cdcbc55bb675da894b1d2fff80eb60eaf
gustawdaniel <gustaw.daniel@gmail.com>
2017-08-15 00:09:10 +0200

    Install updated

--- install.sh
+++ install.sh
@@ -12,3 +12,9 @@ sudo apt install sqlite3 libcurl4-gnutls-dev pkg-config -y
 cpan install Net::Curl::Easy Net::Curl::Multi DBD::SQLite
 
 bash db.sh
+
+# sudo apt install pbzip2
+
+# time tar -cf res/all_100000.tar.bz2 --use-compress-prog=pbzip2 raw/all_100000/
+
+# rsync -r --info=progress2 raw/all.bak/all/ raw/all
