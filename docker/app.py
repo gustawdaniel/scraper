085511be8ab64ba9e434a38091b83bd7f954d3b4
gustawdaniel <gustaw.daniel@gmail.com>
2017-11-05 15:51:26 +0100

    End of docker tutorial

--- docker/app.py
+++ docker/app.py
@@ -15,10 +15,10 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
+    html = "<h3>Hello {name}!!!</h3>\n" \
+           "<b>Hostname:</b> {hostname}<br/>\n" \
+           "<b>Visits:</b> {visits}\n"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=80)
